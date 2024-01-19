13dcb2a610e162858a897306ca67910e82d2890b
ShadowyBlue <alandchung09@gmail.com>
2024-01-18 20:56:39 -0600

    completed studio exercises - strings/arrays

--- arrays/studio/array-string-conversion/array-testing.js
+++ arrays/studio/array-string-conversion/array-testing.js
@@ -6,41 +6,80 @@ let protoArray4 = "Comma-spaces, might, require, typing, caution";
 strings = [protoArray1, protoArray2, protoArray3, protoArray4];
 
 //2) 
+
+
 function reverseCommas() {
 	//TODO: 1. create and instantiate your variables.
-	let check;
+	let str = protoArray1;
+	let check = str.includes(',');
 	let output;
+	let tempArray;
 	//TODO: 2. write the code required for this step
-
+	if (check) {
+		tempArray = str.split(',');
+		tempArray.reverse();
+		output = tempArray.join(',');
+	}
+	else {
+		output = "String does not have a comma.";
+	}
 	//NOTE: For the code to run properly, you must return your output.  this needs to be the final line of code within the function's { }.
 	return output;
 }
 
 //3)
 function semiDash() {
-	let check;
+	let str = protoArray2;
+	let check = str.includes(';');
 	let output;
-//TODO: write the code required for this step
-
+	let tempArray;
+	//TODO: 2. write the code required for this step
+	if (check) {
+		tempArray = str.split(';');
+		tempArray.sort();
+		output = tempArray.join('-');
+	}
+	else {
+		output = "String does not have a semicolon.";
+	}
   
 	return output;
 }
 
 //4)
 function reverseSpaces() {
-	let check;
+	let str = protoArray3;
+	let check = str.includes(' ');
 	let output;
-  //TODO: write the code required for this step
-
+	let tempArray;
+	//TODO: 2. write the code required for this step
+	if (check) {
+		tempArray = str.split(' ');
+		tempArray.sort();
+		tempArray.reverse();
+		output = tempArray.join(' ');
+	}
+	else {
+		output = "String does not have a space.";
+	}
 	return output;
 }
 
 //5)
 function commaSpace() {
-	let check;
+	let str = protoArray4;
+	let check = str.includes(', ');
 	let output;
-	//TODO: write the code required for this step
-  
+	let tempArray;
+	//TODO: 2. write the code required for this step
+	if (check) {
+		tempArray = str.split(', ');
+		tempArray.reverse();
+		output = tempArray.join(',');
+	}
+	else {
+		output = "String does not have a comma space.";
+	}
 	return output;
 }
 
--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -1,11 +1,29 @@
 const input = require('readline-sync');
 let str = "LaunchCode";
 
+// Lau nchCode
+// 012 3456789
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 //Hint - define another variable to hold the new string or reassign the new string to str.
-
+newStr = str.slice(3) + str.slice(0,3);
+// console.log(str);
 //Use a template literal to print the original and modified string in a descriptive phrase.
+//console.log(`Original string: ${str} \n--------------------------------\nNew string: ${newStr}\n--------------------------------`);
+
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
+let userNum = input.question('Number of letters to relocate? ');
+// console.log(userNum);
+
+let errorMessage = "";
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
+if (userNum > str.length) {
+    newStr = str.slice(3) + str.slice(0,3);
+    errorMessage = "\nInput number too high. Defaulting to moving (3) characters.\n--------------------------------"
+}
+else {
+    newStr = str.slice(userNum) + str.slice(0,userNum);
+}
+
+console.log(`${errorMessage}\nOriginal string:   ${str} \n--------------------------------\nNew string:        ${newStr}`);
