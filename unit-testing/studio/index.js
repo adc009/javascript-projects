5ebde201ea7a6b9ff757fe0b9b271cbccd17bb73
ShadowyBlue <alandchung09@gmail.com>
2024-02-08 20:17:26 -0600

    completed studio exercises for unit testing

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,53 @@
 
 let launchcode = {
-  
+  'organization': 'nonprofit',
+  'executiveDirector': 'Jeff',
+  'percentageCoolEmployees': 100,
+  'programsOffered': ["Web Development", "Data Analysis", "Liftoff"],
+  'launchOutput': launchOutput,
+}
+
+
+/*
+When passed a number that is ONLY divisible by 2, return 'Launch!'
+When passed a number that is ONLY divisible by 3, return 'Code!'
+When passed a number that is ONLY divisible by 5, return 'Rocks!'
+When passed a number that is divisible by 2 AND 3, return 'LaunchCode!'
+When passed a number that is divisible by 3 AND 5, return 'Code Rocks!'
+When passed a number that is divisible by 2 AND 5, return 'Launch Rocks!'
+When passed a number that is divisible by 2, 3, AND 5, return 'LaunchCode Rocks!'
+When passed a number that is NOT divisible by 2, 3, or 5, return 'Rutabagas! That doesn't work.'
+*/
+function launchOutput(number) {
+    let two = (number % 2 === 0);
+    let three = (number % 3 === 0);
+    let five = (number % 5 === 0);
+    let output = "";
+
+    if (!two && !three && !five) {
+        return "Rutabagas! That doesn't work.";
+    }
+    if (two && five && !three) {
+        return 'Launch Rocks! (CRASH!!!!)';
+    }
+    if (two) {
+        output += "Launch";
+    }
+    if (three) {
+        output += "Code";
+    }
+    if (five) {
+        if (!two && !three) {
+            output += "Rocks";
+        }
+        else {
+            output += " Rocks";
+        }
+    }
+    output += "!";
+    return output;
+
+
 }
 
 module.exports = launchcode;
-
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,60 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
+  test("organization returns 'nonprofit'", function() {
+    expect(launchcode.organization).toBe("nonprofit");
+  });
+  test("executiveDirector returns 'Jeff'", function() {
+    expect(launchcode.executiveDirector).toBe("Jeff");
+  });
+  test("percentageCoolEmployees returns 100", function() {
+    expect(launchcode.percentageCoolEmployees).toBe(100);
+  });
+  test("programsOffered contains Web Development, Data Analysis, and Liftoff and is size 3", function() {
+    expect(launchcode.programsOffered).toContain("Web Development");
+    expect(launchcode.programsOffered).toContain("Data Analysis");
+    expect(launchcode.programsOffered).toContain("Liftoff");
+    expect(launchcode.programsOffered.length).toBe(3);
+
+  });
   
+  test("launchOutput - When passed a number that is ONLY divisible by 2, return 'Launch!'", function() {
+    expect(launchcode.launchOutput(4)).toBe("Launch!");
+    
+  });
+  test("launchOutput - When passed a number that is ONLY divisible by 3, return 'Code!'",function() {
+    expect(launchcode.launchOutput(9)).toBe("Code!");
+
+  });
+  test("launchOutput - When passed a number that is ONLY divisible by 5, return 'Rocks!'",function() {
+    expect(launchcode.launchOutput(25)).toBe('Rocks!');
+
+  });
+  test("launchOutput - When passed a number that is divisible by 2 AND 3, return 'LaunchCode!'",function() {
+    expect(launchcode.launchOutput(6)).toBe('LaunchCode!');
+
+  });
+  test("launchOutput - When passed a number that is divisible by 3 AND 5, return 'Code Rocks!'",function() {
+    expect(launchcode.launchOutput(15)).toBe('Code Rocks!');
+
+  });
+  test("launchOutput - When passed a number that is divisible by 2 AND 5, return 'Launch Rocks! (CRASH!!!!)'",function() {
+    expect(launchcode.launchOutput(10)).toBe('Launch Rocks! (CRASH!!!!)');
+
+  });
+  test("launchOutput - When passed a number that is divisible by 2, 3, AND 5, return 'LaunchCode Rocks!'",function() {
+    expect(launchcode.launchOutput(30)).toBe('LaunchCode Rocks!');
+
+  });
+  test("launchOutput - When passed a number that is NOT divisible by 2, 3, or 5, return 'Rutabagas! That doesn't work.'",function() {
+    expect(launchcode.launchOutput(7)).toBe("Rutabagas! That doesn't work.");
+
+  });
+  
+
 });
+
+/*
+["Web Development", "Data Analysis", "Liftoff"]
+
+*/
