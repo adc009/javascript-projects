da8e7014581706bddf859c5772a893535fc7aa36
ShadowyBlue <alandchung09@gmail.com>
2024-02-12 15:16:38 -0600

    completed exceptions exercises

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,24 +1,53 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
+    let result;
+    try {
+      result = lab.runLab(3);
+      console.log(`${lab.student} code worked: ${result === 27}`);
+    } catch(err) {
+      result = "Error thrown.";
+      console.log(`${lab.student} code worked: ${result}`);
+    }   
   }
 }
 
-let studentLabs = [
+// let studentLabs = [
+//   {
+//     student: 'Carly',
+//     runLab: function (num) {
+//         return Math.pow(num, num);
+//     }
+//   },
+//   {
+//     student: 'Erica',
+//     runLab: function (num) {
+//         return num * num;
+//     }
+//   }
+// ];
+
+let studentLabs2 = [
   {
-    student: 'Carly',
-    runLab: function (num) {
+      student: 'Blake',
+      myCode: function (num) {
         return Math.pow(num, num);
-    }
+      }
   },
   {
-    student: 'Erica',
-    runLab: function (num) {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
         return num * num;
-    }
+      }
   }
 ];
 
-gradeLabs(studentLabs);
+gradeLabs(studentLabs2);
+
+// gradeLabs(studentLabs);
