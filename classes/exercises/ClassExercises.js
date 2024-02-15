bf0c82d9b0e0ddd87fca6d909a6c961a5a58b08d
ShadowyBlue <alandchung09@gmail.com>
2024-02-15 17:43:14 -0600

    completed exercises

--- classes/exercises/ClassExercises.js
+++ classes/exercises/ClassExercises.js
@@ -1,10 +1,55 @@
 // Define your Book class here:
+class Book {
+    constructor (title, author, copyrightDate, isbn, numPages, timesCheckedOut, discarded) {
+        this.title = title;
+        this.author = author;
+        this.copyrightDate = copyrightDate;
+        this.isbn = isbn;
+        this.numPages = numPages;
+        this.timesCheckedOut = timesCheckedOut;
+        this.discarded = discarded;
+
+    }
+
+    checkOut(times = 1) {
+        this.timesCheckedOut+= times;
+    }
+
 
 
 // Define your Manual and Novel classes here:
+}
 
+class Manual extends Book {
+    constructor(title, author, copyright, isbn, pages, timesCheckedOut, discarded){
+        super(title, author, copyright, isbn, pages, timesCheckedOut, discarded);
+    }
+
+    discard(currentYear) {
+        if (currentYear - this.copyrightDate > 5) {
+            this.discarded = 'Yes';
+        }
+    }
+    
+}
+
+class Novel extends Book {
+    constructor(title, author, copyright, isbn, pages, timesCheckedOut, discarded){
+        super(title, author, copyright, isbn, pages, timesCheckedOut, discarded);
+    }
+
+    discard() {
+        if (this.timesCheckedOut > 100) {
+            this.discarded = 'Yes';
+        }
+    }
+}
 
 // Declare the objects for exercises 2 and 3 here:
+let prideAndPrejudice = new Novel('Pride and Prejudice', 'Jane Austen', 1813, '1111111111111', 432, 32, 'No');
+let shuttleManual = new Manual('Top Secret Shuttle Building Manual', 'Redacted', 2013, '0000000000000', 1147, 1, 'No');
 
 
 // Code exercises 4 & 5 here:
+prideAndPrejudice.discard();
+shuttleManual.checkOut(5);
